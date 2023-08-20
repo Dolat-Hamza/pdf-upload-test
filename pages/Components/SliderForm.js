fcf42ad877a4abd000a118b2372f1836e7776cbd
Dolat Hamza <dolathamza8019@gmail.com>
2023-08-20 23:40:02 +0500

    Added form in sidebar

---
+++ pages/Components/SliderForm.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {Slider, Form, Button} from 'antd';
+import {QuestionCircleOutlined} from "@ant-design/icons";
+
+const SliderForm = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log('Slider values:', values);
+    };
+
+    return (
+        <Form
+            form={form}
+            name="sliderForm"
+            onFinish={onFinish}
+            initialValues={{
+                slider1: 50,
+                slider2: 50,
+                slider3: 50,
+            }}
+        >
+            <div className={"flex flex-col"}>
+                <div className={"flex flex-row items-center justify-between"}>
+                    <div>
+                        Enter the chunk size. (Default: 1250)
+                    </div>
+                    <QuestionCircleOutlined/>
+                </div>
+                <Form.Item className={"w-full"} name="slider1">
+                    <Slider/>
+                </Form.Item>
+            </div>
+            <div className={"flex flex-col"}>
+                <div className={"flex flex-row items-center justify-between"}>
+                    <div>
+                        Enter the chunk overlap. (Default: 150)
+                    </div>
+                    <QuestionCircleOutlined/>
+                </div>
+                <Form.Item className={"w-full"} name="slider1">
+                    <Slider/>
+                </Form.Item>
+            </div>
+
+            <div className={"flex flex-col"}>
+                <div className={"flex flex-row items-center justify-between"}>
+                    <div>
+                        Set the AI model temperature. (Default: 0.1)
+                    </div>
+                    <QuestionCircleOutlined/>
+                </div>
+                <Form.Item className={"w-full"} name="slider1">
+                    <Slider/>
+                </Form.Item>
+            </div>
+
+
+        </Form>
+    );
+};
+
+export default SliderForm;
